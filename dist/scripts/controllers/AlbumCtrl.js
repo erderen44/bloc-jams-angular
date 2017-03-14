8d531868233b0b9915823d13b1774ff0ebf7f0d2
Jonathan <jonathan_whitfield@hotmail.com>
2017-03-14 23:36:25 +0800

    assignment-5, half-way completed because stumped

---
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,11 @@
+(function() {
+     function AlbumCtrl() {
+             this.albumData = angular.copy.(albumPicasso); 
+            // this.albums.push(angular.copy(albumPicasso));//
+            
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', AlbumCtrl);
+ })();
